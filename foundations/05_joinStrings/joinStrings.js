c5baffecd9a64c1fb9640dc724f79534fc143320
Cherif <227361371+chtre-js@users.noreply.github.com>
2026-05-25 17:56:21 +0200

    Resolve exercise

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -6,9 +6,16 @@
 
  The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
 */
-
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947;
+let fullName = firstName + " " + lastName;
+let age = thisYear - birthYear;
 // Add your code right below, good luck!
 
+
+let greeting = "Hello! My name is " + fullName + " and I am " + age + " years old."
 
 
 
