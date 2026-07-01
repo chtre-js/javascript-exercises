f50df97c9fc5a39ec19fd5ed2c4aad67d7b73254
Cherif <227361371+chtre-js@users.noreply.github.com>
2026-07-02 01:33:58 +0200

    Implement 'repeatString' function

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(str, val) {
+    if (val < 0) console.log("ERROR");
+    
+    let output = "";
+    let i = 0;
+    while (i < val) {
+        output += str;
+        i++;
+    }
+    console.log(output)
 };
+
+repeatString("Hello", 3);
+
 
 // Do not edit below this line
 module.exports = repeatString;
