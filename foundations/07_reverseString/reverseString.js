963b1d94aa2a45deaa94581a0ec46e3cfb1b0a20
Cherif <227361371+chtre-js@users.noreply.github.com>
2026-07-02 01:47:40 +0200

    Implement function

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  let arr = str.toLowerCase().split("");
+  let reversedArr = [];
 
+  arr.forEach((element) => {
+    reversedArr.unshift(element);
+  });
+
+  output = reversedArr.join("");
+  return output;
 };
+
+console.log(reverseString("Hello there"));
 
 // Do not edit below this line
 module.exports = reverseString;
