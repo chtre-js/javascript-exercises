f2620530063245d8af280aaf47124a2bd90a0372
Cherif <227361371+chtre-js@users.noreply.github.com>
2026-07-02 22:47:55 +0200

    Implement function

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let converted = (fahrenheit - 32) * (5/9);
+  let output = parseFloat(converted.toFixed(1));
+  return output;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let converted = celsius * (9/5) + 32;
+  let output = parseFloat(converted.toFixed(1));
+  return output;
 };
 
 // Do not edit below this line
