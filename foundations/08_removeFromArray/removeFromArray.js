daf3c3f202101404fd9b8066c1f7c5f9aca779dc
Cherif <227361371+chtre-js@users.noreply.github.com>
2026-07-02 19:07:35 +0200

    Implement function

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, arg) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === arg) {
+            arr.splice(i, 1);
+        }
+    }
+    return arr
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3)); // should remove 3 and return [1,2,4]
 
 // Do not edit below this line
 module.exports = removeFromArray;
