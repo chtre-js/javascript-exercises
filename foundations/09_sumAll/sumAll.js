f4e9922af47acf1702c2221271b122e5a443e261
Cherif <227361371+chtre-js@users.noreply.github.com>
2026-07-02 19:47:44 +0200

    Implement function

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let start = a, end = b;
 
+    if (a > b) [start, end] = [end, start];
+
+    let arr = [];
+
+    for (let i = start; i <= end; i++) {
+        arr.push(i);
+    }
+
+    let output = arr.reduce((sum, current) => sum + current, 0);
+
+    return output;
 };
+
+console.log(sumAll(100, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
