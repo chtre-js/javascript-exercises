71b438c94fc0de1c19efa1eebebe57cf7237c4d1
chtre-js <227361371+chtre-js@users.noreply.github.com>
2026-08-18 18:07:28 +0200

    feat: Implement recursive integer counting function

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,15 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function(target) {
+  if (typeof target !== "object" || target === null) {
+    return undefined;
+  }
+
+  return Object.values(target).reduce((count, value) => {
+    if (typeof value === "object" && value !== null) {
+      return count + totalIntegers(value);
+    }
+    return count + (Number.isInteger(value) ? 1 : 0);
+  }, 0)
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
