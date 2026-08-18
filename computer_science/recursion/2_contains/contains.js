d9ad672f7a22c494645d832b2164d1f21dc41fdb
chtre-js <227361371+chtre-js@users.noreply.github.com>
2026-08-18 17:20:00 +0200

    feat: Implement recursive object search

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,32 @@
-const contains = function() {
-  
+const contains = function(object, value) {
+  if (typeof object !== "object") {
+    return undefined;
+  }
+  if (Object.values(object).includes(value)) {
+    return true;
+  } 
+  for (const subobject of Object.values(object)) {
+    if (typeof subobject === "object" && subobject !== null && contains(subobject, value)) {
+      return true;
+    }
+  }
+  return false;
 };
-  
+
+const object = {
+  "one": 1,
+  "two": 2,
+  "sub": {
+    "three": 3,
+    "four": 4,
+    "yes": "yes",
+    "number": "44",
+  }
+}
+
+console.log(contains(object, 3));
+console.log(contains(object, "yes"));
+console.log(contains(object, 4));
+console.log(contains(object, 44));
 // Do not edit below this line
 module.exports = contains;
