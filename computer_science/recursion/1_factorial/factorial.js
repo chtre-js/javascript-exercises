6b392986558a2429c7ba1eb656879e24be22c696
chtre-js <227361371+chtre-js@users.noreply.github.com>
2026-08-18 15:17:34 +0200

    Resolve exercise

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,6 +1,18 @@
-const factorial = function() {
+function isFloat(n) {
+  return Number(n) === n && n % 1 !== 0;
+}
 
+const factorial = function (n) {
+  if (typeof n === "object" || typeof n === "string" || n < 0 || isFloat(n)) {
+    return undefined;
+  }
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 };
+
+console.log(factorial(1));
 
 // Do not edit below this line
 module.exports = factorial;
